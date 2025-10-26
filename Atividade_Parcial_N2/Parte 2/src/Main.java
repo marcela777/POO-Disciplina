82c6b1f251cca9e1c38520a32a1786cf76aa5685
Marcela <marceladoaraujo@gmail.com>
2025-10-26 17:40:47 -0400

    Adicao Classe Funcao Principal

--- Atividade_Parcial_N2/Parte 2/src/Main.java
+++ Atividade_Parcial_N2/Parte 2/src/Main.java
@@ -2,14 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Agenda agenda = new Agenda("Victor", 10);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Pessoa ana = new Pessoa("Ana", 5);
+        ana.adicionarContato(new Email("ana@mail.com"));
+        ana.adicionarContato(new Telefone("92993599234"));
+
+        Pessoa bruno = new Pessoa("Bruno", 5);
+        bruno.adicionarContato(new Telefone("92988026481"));
+
+        agenda.addPessoa(ana);
+        agenda.addPessoa(bruno);
+
+        agenda.addContato("Bruno", new Email("bruno@mail.com"));
+
+        agenda.exibirTodasPessoas();
+        System.out.println();
+
+        agenda.exibirContatosPessoa("Ana");
+        System.out.println();
+
+        agenda.exibirPessoasComEmail();
+        System.out.println();
+
+        agenda.recuperarPessoaPorEmail("bruno@mail.com");
+        agenda.recuperarPessoaPorTelefone("92993599234");
     }
 }
